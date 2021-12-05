6c5164fa7040788a8df749b76fc46c98c48b5c84
BeaniBum <60374872+BeaniBum@users.noreply.github.com>
2021-12-04 22:48:25 -0700

    Lots of changes
    -Changed menu scene name to start screen
    -set tictacktoe scene name to playscreen
    -PlayScreen Sceen Changes-
    Canvas Scaler Changes
    -set the canvas to in play screen to screen space overlay
    -set the ui scale mode to scale with screen size
    -set resolution to 1024/768
    set match to  1
    ----------
    Ar Session Origin
    -Tagged Ar Session Origin as Main Camera
    -Set ar prefab plane to checker
    -------
    Added SceneLoader script

---
+++ ArTake2/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public enum Scene
+    {
+        StartScreen,
+        PlayScreen
+    }
+
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+}
