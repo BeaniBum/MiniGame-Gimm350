0c9ea24713948fafa26387886c29731e98227490
Maxx-Mouse <59904005+Maxx-Mouse@users.noreply.github.com>
2021-12-06 19:03:28 -0700

    bttn manger and orginization'

---
+++ ArTake2/Assets/Scripts/BttnMangers/StartScreenBttnManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScreenBttnManager : MonoBehaviour
+{
+    public void StartPlayScreen()
+    {
+        SceneManager.LoadScene("PlayScreen");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
